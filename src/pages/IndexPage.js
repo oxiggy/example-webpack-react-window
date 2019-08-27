c49bb5c7ce0daad326e2877fc0c41e29b2334a19
Ada Void <oxiggy@oxiggy.com>
2019-08-27 17:24:27 +0200

    начало

---
+++ src/pages/IndexPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default class IndexPage extends React.PureComponent {
+
+    render() {
+        const {  }= this.props
+
+        return (
+            <div>
+                index page here!
+            </div>
+        )
+    }
+
+}
