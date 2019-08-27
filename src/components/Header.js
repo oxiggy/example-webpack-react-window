025e0d4fead70167eaf87f91a239ce69c4bb294c
Ada Void <oxiggy@oxiggy.com>
2019-08-27 21:16:45 +0200

    шапка и поиск

---
+++ src/components/Header.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import { withStyles } from '@material-ui/styles'
+import { fade } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Typography from '@material-ui/core/Typography'
+import InputBase from '@material-ui/core/InputBase'
+
+import SearchIcon from '@material-ui/icons/Search'
+
+
+import Box from '@material-ui/core/Box'
+
+
+class Header extends React.PureComponent {
+
+    state= {
+        userMenuOpen: false,
+    }
+
+    handleOpenUserMenu= () => {
+        this.setState({ userMenuOpen:true })
+    }
+
+    handleCloseUserMenu= () => {
+        this.setState({ userMenuOpen:false })
+    }
+
+    render() {
+        const { classes, onOpenMenu, keyword, onChangeKeyword }= this.props
+        return (
+            <AppBar position="static" color="inherit" elevation={0} className={classes.root}>
+                <Box className={classes.fix}/>
+                <Toolbar>
+                    <IconButton
+                        color="primary"
+                        edge="start"
+                        aria-label="menu"
+                        onClick={onOpenMenu}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <div style={{ flexGrow:1 }}/>
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+                        <InputBase
+                            value={keyword}
+                            placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                            onChange={onChangeKeyword}
+                        />
+                    </div>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+
+}
+
+const styles= (theme) => ({
+    root: {
+        position: 'relative',
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.background.default, 0.75),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.background.default, 1),
+        },
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(1),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing(7),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 7),
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            width: 120,
+            '&:focus': {
+                width: 200,
+            },
+        },
+    },
+})
+
+export default withStyles(styles)(
+    Header
+)
+
--- src/pages/IndexPage.js
+++ src/pages/IndexPage.js
@@ -8,6 +8,16 @@ import ListItem from '@material-ui/core/ListItem'
 import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import Avatar from '@material-ui/core/Avatar'
 import ListItemText from '@material-ui/core/ListItemText'
+import { Box } from '@material-ui/core'
+import Header from '../components/Header'
+
+const list= Object
+    .keys(colors)
+    .map((key) => ({
+        name: key,
+        color: colors[key],
+    }))
+;
 
 const Row= ({ index, style, data:{ classes, list, onClick } }) => (
     <ListItem
@@ -27,39 +37,64 @@ const Row= ({ index, style, data:{ classes, list, onClick } }) => (
 
 class IndexPage extends React.PureComponent {
 
+    state= {
+        keyword: '',
+        filteredList: [],
+    }
+
+    componentDidMount() {
+        this.filterList()
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.state.keyword !== prevState.keyword) {
+            this.filterList()
+        }
+    }
+
+    filterList() {
+        const filteredList= list.filter((item) => item.name.indexOf(this.state.keyword) > -1)
+        this.setState({ filteredList })
+    }
+
     render() {
         const { classes }= this.props
-        const list= Object
-            .keys(colors)
-            .map((key) => ({
-                name: key,
-                color: colors[key],
-            }))
-        ;
         return (
-            <div className={classes.root}>
-                <AutoSizer>
-                    {({width, height}) => (
-                        <List
-                            className={classes.list}
-                            width={width}
-                            height={height}
-                            itemCount={list.length}
-                            itemSize={48}
-                            itemData={{
-                                classes,
-                                list,
-                                onClick: this.handleClick,
-                            }}
-                            innerElementType="ul"
-                            overscanCount={10}
-                        >
-                            {Row}
-                        </List>
-                    )}
-                </AutoSizer>
-            </div>
+            <React.Fragment>
+
+                <Box>
+                    <Header keyword={this.state.keyword} onChangeKeyword={this.handleChangeKeyword}/>
+                </Box>
+
+                <Box className={classes.root}>
+                    <AutoSizer>
+                        {({width, height}) => (
+                            <List
+                                className={classes.list}
+                                width={width}
+                                height={height}
+                                itemCount={this.state.filteredList.length}
+                                itemSize={48}
+                                itemData={{
+                                    classes,
+                                    list: this.state.filteredList,
+                                    onClick: this.handleClick,
+                                }}
+                                innerElementType="ul"
+                                overscanCount={10}
+                            >
+                                {Row}
+                            </List>
+                        )}
+                    </AutoSizer>
+                </Box>
+
+            </React.Fragment>
         )
+    }
+
+    handleChangeKeyword= ({ target:{ value:keyword }}) => {
+        this.setState({ keyword })
     }
 
     handleClick= (item, index) => {
@@ -74,11 +109,7 @@ class IndexPage extends React.PureComponent {
 
 const styles= (theme) => ({
     root: {
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
+        flexGrow: 1,
     },
     list: {
         '& > ul': {
